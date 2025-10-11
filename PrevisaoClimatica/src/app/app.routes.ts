ff7db0dd12a3f401e684a441177131e28a77dff6
Bruno <Bruno@DESKTOP-8FQFVA0>
2025-10-11 17:53:05 -0300

    Feat - Rotas configuradas

--- PrevisaoClimatica/src/app/app.routes.ts
+++ PrevisaoClimatica/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//componentes criados
+import { Login } from './components/login/login';
+import { Home } from './components/home/home';
+import { Detalhes } from './components/detalhes/detalhes';
+import { Favoritos } from './components/favoritos/favoritos';
+
+//array de rotas
+export const routes: Routes = [
+  // Rota de Login
+  { path: 'login', component: Login },
+
+  // Rota Home
+  { path: 'home', component: Home },
+
+  // Rota Detalhes (usando parâmetro 'cidade')
+  { path: 'detalhes/:cidade', component: Detalhes },
+
+  // Rota Favoritos
+  { path: 'favoritos', component: Favoritos },
+
+  // Rota Padrão: Redireciona a URL base ('') para o Login
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+  // Redireciona tudo que não for mapeado para a Home
+  { path: '**', redirectTo: '/home' } 
+];
